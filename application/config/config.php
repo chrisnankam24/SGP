48cb2f30f0aa0dfefb7a8364fa108c05c3a49fe8
chrisnankam24 <bamenda24>
2017-01-09 13:30:18 +0100

    09/01/17

--- application/config/config.php
+++ application/config/config.php
@@ -370,8 +370,8 @@ $config['encryption_key'] = '#SGP*2016%ORANGE$CM';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
-$config['sess_match_ip'] = FALSE;
+$config['sess_save_path'] = substr(BASEPATH, 0, count(BASEPATH) - 8) . 'data/sessions';
+$config['sess_match_ip'] = TRUE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
 
